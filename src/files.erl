a417aa1a840dc3c0f576332ebcc9bac1c23e8e54
Barney Govan <barney@thegovans.us>
2011-04-17 00:52:57 -0700

    Added file filtering and ignore swap files

--- src/files.erl
+++ src/files.erl
@@ -1,7 +1,7 @@
 -module(files).
 -include_lib("kernel/include/file.hrl").
 
--export([walk_file_tree/1, get_added_files/2]).
+-export([get_file_list/1, get_file_list/2, get_added_files/2, get_deleted_files/2, get_filtered_file_list/2, get_filtered_file_list/3]).
 
 gen_file_signature(Filename, F) ->
     {SignatureResult, Data} = F(Filename),
@@ -35,9 +35,12 @@ is_symlink(Filename) ->
         _ -> false
     end.
 
-walk_file_tree([], L) -> 
+walk_file_tree(Files, L) ->
+    walk_file_tree(Files, L, nil, []).
+
+walk_file_tree([], L, _, _) -> 
     L;
-walk_file_tree([File|Rest], L) when is_list(File) ->
+walk_file_tree([File|Rest], L, FilterFun, IgnoreList) when is_list(File) ->
     case is_symlink(File) of 
         true ->
             walk_file_tree(Rest, L);
@@ -45,7 +48,12 @@ walk_file_tree([File|Rest], L) when is_list(File) ->
             case filelib:is_dir(File) of
                 true -> 
                     {ok, Children} = file:list_dir(File),
-                    ChildrenPaths = lists:map(add_this_path(File), Children),
+                    case FilterFun == nil of
+                        false ->
+                            ChildrenPaths = lists:map(add_this_path(File), filter_files(FilterFun,IgnoreList,Children));
+                        true ->
+                            ChildrenPaths = lists:map(add_this_path(File), Children)
+                        end,
                     DirList = walk_file_tree(ChildrenPaths, L),
                     walk_file_tree(Rest, DirList);
                 false -> 
@@ -59,9 +67,44 @@ walk_file_tree([File|Rest], L) when is_list(File) ->
              end
     end.
 
-walk_file_tree(File) ->
-    Sigs = walk_file_tree([File], []),
+get_file_list(File,FilterFun,IgnoreList) ->
+    Sigs = walk_file_tree([File],[],FilterFun,IgnoreList),
     lists:sort(fun({X,_,_,{{_,_,_},{_,_,_}}},{Y,_,_,{{_,_,_},{_,_,_}}}) -> X < Y end, Sigs).
+
+get_file_list(File,relative,FilterFun,IgnoreList) ->
+    case filelib:is_dir(File) of
+        true ->
+            shell_default:cd(File),
+            get_file_list(".", FilterFun,IgnoreList);
+        false ->
+            {error, "You must supply a path to a root directory"}
+    end;
+get_file_list(File,absolute,FilterFun,IgnoreList) ->
+    get_file_list(File,FilterFun,IgnoreList).
+
+get_file_list(File,PathType)->
+    get_file_list(File,PathType,nil,[]).
+
+get_file_list(File) ->
+    get_file_list(File, relative).
+
+get_filtered_file_list(File,IgnoreList) ->
+    get_filtered_file_list(File,IgnoreList,relative).
+
+get_filtered_file_list(File,IgnoreList,PathType) ->
+    DontMatchMe = fun(Pat) -> (fun(X) -> case re:run(X, Pat) of {match,_}-> false; nomatch->true end end) end,
+    get_file_list(File,PathType,DontMatchMe,IgnoreList).
+
+filter_files(_, [], L) ->
+    lists:reverse(L);
+filter_files(FilterFun, [CurrentPattern|Rest], L) ->
+    {Result, Regexp} = re:compile(CurrentPattern),
+    case Result of
+        ok ->
+            filter_files(FilterFun, Rest, lists:filter(FilterFun(Regexp), L));
+        Error -> Error
+    end.
+    
 
 add_this_path(Path) ->
     (fun(Filename) -> Path ++ "/" ++ Filename end).
@@ -74,15 +117,15 @@ last_write_time(Filename) ->
         Error -> Error 
     end.
 
-get_added_files(OldFileSet,File) ->
-    NewFileList = walk_file_tree(File),
+get_added_files(OldFileList,File) ->
+    OldFileSet = sets:from_list(OldFileList),
+    NewFileList = get_file_list(File),
     NewFileSet = sets:from_list(NewFileList),
-    sets:subtract(NewFileSet, OldFileSet).
+    sets:to_list(sets:subtract(NewFileSet, OldFileSet)).
 
-%MyList = files:walk_file_tree("/home/barney/git/fs/src").
-%MySet = sets:from_list(MyList).
-%MyNewList = files:walk_file_tree("/home/barney/git/fs/src").
-%MyNewSet = sets:from_list(MyNewList).
-%DiffSet = sets:subtract(MyNewSet, MySet).
-%DiffSet2 = sets:subtract(MySet, MyNewSet).
-%sets:to_list(DiffSet2).
+get_deleted_files(OldFileList,File) ->
+    OldFileSet = sets:from_list(OldFileList),
+    NewFileList = get_file_list(File),
+    NewFileSet = sets:from_list(NewFileList),
+    sets:to_list(sets:subtract(OldFileSet, NewFileSet)).
+
