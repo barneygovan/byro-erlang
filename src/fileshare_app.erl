1de6d0b847c7e87f19deca141751a4c71986ea4b
barney <barney@thegovans.us>
2011-11-13 14:23:13 -0800

    Rearranging, and cleaning up

--- src/fileshare_app.erl
+++
@@ -1,9 +0,0 @@
--module(fileshare_app).
--behaviour(application).
--export([start/2,stop/1]).
-
-start(_Type, StartArgs) ->
-    fileshare_supervisor:start_link(StartArgs).
-
-stop(_State) ->
-    ok.
--- src/user_default.erl
+++
@@ -1,8 +0,0 @@
--module(user_default).
-
--export([la/0]).
-
-la() -> 
-    Modules = [M || {M, P} <- code:all_loaded(), is_list(P) andalso string:str(P, "fs/ebin") > 0],
-    [shell_default:l(M) || M <- Modules].
-
